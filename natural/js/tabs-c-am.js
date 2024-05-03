ecbd562bc09d57b68f9c197c589363be98ac1898
mfa33015145 <79053249+mfa33015145@users.noreply.github.com>
2024-05-03 10:03:06 -0300

    Add files via upload

---
+++ natural/js/tabs-c-am.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+function abrirPaginaindex() {
+    window.location.href = "index.html";
+}
+function abrirPaginaFactories() {
+    window.location.href = "factories.html";
+}
+
+function abrirPaginaTabs() {
+    window.location.href = "tabs.html";
+}
+
+function abrirPaginaPosts() {
+    window.location.href = "posts.html";
+}
+
+function avancar(element) {
+    element.style.transform = 'translate(5px, -5px)';
+}
+
+function recuar(element) {
+    element.style.transform = 'translate(0, 0)';
+}
+function playSoundOnHover(somBotao1) {
+    var audio = document.getElementById(somBotao1);
+    if (audio) {
+        audio.currentTime = 0; // Reinicia o áudio, se já estiver reproduzindo
+        audio.play();
+    } else {
+        console.error("Elemento de áudio não encontrado com o ID:", somBotao1);
+    }
+}
